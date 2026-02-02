92c9d78b947bf4418cd45a9fb2e41af70a303f18
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2026-02-02 05:04:17 +0100

    enregistrement scene add

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ const distPath = path.resolve(__dirname, '..', 'dist')
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', clientOrigin)
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   if (req.method === 'OPTIONS') {
     res.sendStatus(204)
     return
